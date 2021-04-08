fa024bc33270dd9f19b8146aaf2a1f7c5c44a1ad
DoteAdore <1379382440@qq.com>
2021-04-08 22:51:55 +0800

    [LevelCreator] 基础实现Map panel的显示效果

--- Source/TowerDefence/LevelEditor/SMapCreatorPanel.cpp
+++ Source/TowerDefence/LevelEditor/SMapCreatorPanel.cpp
@@ -6,7 +6,8 @@
 #include "TowerDefence/GlobalConfig.h"
 #include "TowerDefence/Entities/Base/Tile.h"
 #include "Widgets/Colors/SColorBlock.h"
-
+#include "Widgets/Views/STileView.h"
+#include "TowerDefence/Level/LevelInfomation.h"
 #define LOCTEXT_NAMESPACE "LevelCreatorEditor"
 
 
@@ -71,13 +72,26 @@ void SMapCreatorPanel::Construct(const SMapCreatorPanel::FArguments& InArgs)
 				+SHorizontalBox::Slot()
 				.AutoWidth()
 				[
-					SNew(SBox)
-					.WidthOverride(EachTileSize*15)
-					.HeightOverride(EachTileSize*4)
-					.MaxDesiredWidth(EachTileSize*15)
+					SAssignNew(MapEditorOuterBox,SBox)
+					.WidthOverride_Lambda([&]()->FOptionalSize
+					{
+						return FOptionalSize(OuterBoxWidth);
+					})
+					.HeightOverride_Lambda([&]()->FOptionalSize
+                    {
+                        return FOptionalSize(OuterBoxWidth);
+                    })
 					[
-						SNew(SColorBlock)
-						.Color(FLinearColor::Yellow)
+						SAssignNew(MapEditorTileView, STileView<ABaseTile*>)
+						.ItemWidth(EachTileSize)
+						.ItemHeight(EachTileSize)
+						.OnGenerateTile(this, &SMapCreatorPanel::OnGenerteTileMapItem)
+						.ListItemsSource(&CurrentMapEditorTiles)
+						.OnItemToString_Debug_Lambda([](ABaseTile* InItem)-> FString
+						{
+							return "WTF";
+						})
+						.SelectionMode(ESelectionMode::None)
 					]
 				]
 				+SHorizontalBox::Slot()
@@ -100,6 +114,35 @@ void SMapCreatorPanel::SetCurrentLevelInfo(ULevelInfomation* LevelInformation)
 	if(LevelInformation)
 	{
 	    SNoSelectedTips->SetVisibility(EVisibility::Collapsed);
+		OuterBoxWidth = LevelInformation->TileInfo.Witdh*EachTileSize;
+		OuterBoxHeight = LevelInformation->TileInfo.Height* EachTileSize;
+		// 如果不符合大小，则全部删除重新建立一个
+		int32 AllNum = LevelInformation->TileInfo.Witdh*LevelInformation->TileInfo.Height;
+		if(LevelInformation->TileInfo.Tiles.Num()!=AllNum)
+		{
+			LevelInformation->TileInfo.Tiles.Empty();
+			CurrentMapEditorTiles.Empty();
+			for(int32 i = 0;i<AllNum;i++)
+			{
+				LevelInformation->TileInfo.Tiles.Add(nullptr);
+				CurrentMapEditorTiles.Add(GetMutableDefault<ABaseTile>());
+			}
+			MapEditorTileView->RequestListRefresh();
+			LevelInformation->MarkPackageDirty();
+		}
+		// 从那里面导入相应的defaultObject
+		else
+		{
+			CurrentMapEditorTiles.Empty();
+			for(int32 i = 0; i<AllNum;i++)
+			{
+				UClass* TargetCLass = LevelInformation->TileInfo.Tiles[i];
+				if(TargetCLass)
+					CurrentMapEditorTiles.Add(GetMutableDefault<ABaseTile>(TargetCLass));
+				else
+					CurrentMapEditorTiles.Add(GetMutableDefault<ABaseTile>());
+			}
+		}
 	}
 	else
 	{
@@ -142,5 +185,36 @@ TSharedRef<ITableRow> SMapCreatorPanel::OnGenerateTileTypesRow(const ABaseTile* 
 			]
 		]
 	];
+}
+
+TSharedRef<ITableRow> SMapCreatorPanel::OnGenerteTileMapItem(ABaseTile* InTile,
+	const TSharedRef<STableViewBase>& TableViewBase)
+{
+
+	if(InTile == nullptr)
+	{
+		return SNew(STableRow<const ABaseTile*>, TableViewBase)
+		[
+			SNew(SBorder)
+			[
+				SNew(SColorBlock)
+				.Color(FLinearColor(1,1,1,0.2))
+				.Size(FVector2D(EachTileSize,EachTileSize))
+			]
+		];
+	}
+	else
+	{
+		return SNew(STableRow<const ABaseTile*>, TableViewBase)
+[
+	    SNew(SBorder)
+	    [
+	        SNew(SColorBlock)
+	        .Color(InTile->DebugColor)
+	        .Size(FVector2D(EachTileSize,EachTileSize))
+	    ]
+];
+	}
+
 }
 #undef LOCTEXT_NAMESPACE
--- Source/TowerDefence/LevelEditor/SMapCreatorPanel.h
+++ Source/TowerDefence/LevelEditor/SMapCreatorPanel.h
@@ -1,5 +1,6 @@
 ﻿#pragma once
 #include "TowerDefence/Entities/Base/Tile.h"
+#include "Widgets/Views/STileView.h"
 
 
 class ULevelInfomation;
@@ -20,11 +21,21 @@ private:
 	TSharedPtr<SBorder> SNoSelectedTips;
 	TArray<const ABaseTile*> AllBaseTiles;
 	TSharedPtr<SListView<const ABaseTile*>> TileTypeSelectList;
-
-	int32 EachTileSize = 50.f;
+	TSharedPtr<SBox> MapEditorOuterBox;
+	TSharedPtr<STileView<ABaseTile*>> MapEditorTileView;
+	TArray<ABaseTile*> CurrentMapEditorTiles;
+	TSubclassOf<ABaseTile> EmptyBaseTileClass;
 	
+	int32 EachTileSize = 50;
+	int32 OuterBoxWidth;
+	int32 OuterBoxHeight;
 	
 	void GetDefaultBaseTiles();
 	TSharedRef<ITableRow> OnGenerateTileTypesRow(const ABaseTile* InItem,
 		const TSharedRef<STableViewBase>& TableViewBase);
+	TSharedRef<ITableRow> OnGenerteTileMapItem(ABaseTile* InTile,
+		const TSharedRef<STableViewBase>& TableViewBase);
+	
+	int32 GetOuterBoxWidthOverride();
+	int32 GetOuterBoxHeightOverride();
 };
