709fef1425b776841785094f23297ff41959389d
DoteAdore <1379382440@qq.com>
2021-04-10 20:37:23 +0800

    [LevelCreator] generated ID 界面功能实现

--- Source/TowerDefence/LevelEditor/SWaveSettingsPanel.cpp
+++ Source/TowerDefence/LevelEditor/SWaveSettingsPanel.cpp
@@ -1,6 +1,7 @@
 ﻿#include "SWaveSettingsPanel.h"
 #include "TowerDefence/Level/LevelInfomation.h"
 #include "Widgets/Input/SEditableTextBox.h"
+#include "Widgets/Views/STileView.h"
 #define LOCTEXT_NAMESPACE "LevelCreatorEditor"
 
 void SWaveSettingsPanel::Construct(const SWaveSettingsPanel::FArguments& InArgs,
@@ -8,10 +9,12 @@ void SWaveSettingsPanel::Construct(const SWaveSettingsPanel::FArguments& InArgs,
 {
 	CurrentWaveInfo = EnemyGenerationInfo;
 	CurrentLevelInfomation = ParentLevelInfomation;
+	InitGenerateIDArray();
 	ChildSlot
 	[
 		SNew(SVerticalBox)
 		+SVerticalBox::Slot()
+		.Padding(0,5)
 		.AutoHeight()
 		[
 			SNew(STextBlock)
@@ -23,6 +26,51 @@ void SWaveSettingsPanel::Construct(const SWaveSettingsPanel::FArguments& InArgs,
 		    SAssignNew(GrapTimeInputWidget, SEditableTextBox)
 		    .Text(FText::Format(LOCTEXT("Grap Input Text","{0}"), EnemyGenerationInfo->GrapTime))
 		    .OnTextChanged(this, &SWaveSettingsPanel::OnGrapTimeInputChanged)
+		]
+		+SVerticalBox::Slot()
+		.Padding(0,5)
+		.AutoHeight()
+		[
+		   SNew(SHorizontalBox)
+		   +SHorizontalBox::Slot()
+		   .HAlign(HAlign_Left)
+		   .AutoWidth()
+		   [
+		   		SNew(STextBlock)
+				.Text(LOCTEXT("Generated IDs", "Generated IDs:"))
+		   ]
+		   +SHorizontalBox::Slot()
+		   .FillWidth(1)
+		   +SHorizontalBox::Slot()
+		   .AutoWidth()
+		   .Padding(5,0)
+		   .HAlign(HAlign_Right)
+		   [
+				SNew(SButton)
+                .ButtonStyle(FEditorStyle::Get(), "FlatButton.Success")
+                .ForegroundColor(FSlateColor::UseForeground())
+                .Text(LOCTEXT("Add new Generated ID", "+"))
+                .OnClicked(this, &SWaveSettingsPanel::OnAddNewGeneratedIDBtnClicked)
+		   ]
+		   +SHorizontalBox::Slot()
+           .AutoWidth()
+           .HAlign(HAlign_Right)
+           [
+	           SNew(SButton)
+	            .ButtonStyle(FEditorStyle::Get(), "FlatButton.Danger")
+	            .ForegroundColor(FSlateColor::UseForeground())
+	            .Text(LOCTEXT("Delete Selected Generated ID", "-"))
+	            .OnClicked(this, &SWaveSettingsPanel::OnDeleteSelectedIDBtnClicked)
+           ]
+		]
+		+SVerticalBox::Slot()
+		.AutoHeight()
+		[
+			SAssignNew(GeneratedIDListTileWidget, STileView<FGeneratedIDEntry>)
+			.ItemWidth(60)
+			.ItemHeight(25)
+			.OnGenerateTile(this, &SWaveSettingsPanel::OnGenerateIDTile)
+			.ListItemsSource(&GeneratedIDArray)
 		]
 	];
 }
@@ -39,6 +87,84 @@ void SWaveSettingsPanel::OnGrapTimeInputChanged(const FText& InText)
 	{
 		GrapTimeInputWidget->SetText(FText::Format(LOCTEXT("Grap Input Text","{0}"), CurrentWaveInfo->GrapTime));
 	}
+}
+
+TSharedRef<ITableRow> SWaveSettingsPanel::OnGenerateIDTile(FGeneratedIDEntry InEntry, const TSharedRef<STableViewBase>& TableViewBase)
+{
+	return SNew(STableRow<FGeneratedIDEntry>, TableViewBase)
+	[
+		SNew(SBorder)
+		.Padding(5)
+		.BorderImage(&GenerateIDItemBorderBrush)
+		[
+			SNew(SHorizontalBox)
+			+SHorizontalBox::Slot()
+			.AutoWidth()
+			[
+				SNew(SEditableTextBox)
+		        .Text_Lambda([InEntry]()->FText
+		        {
+		            return FText::FromString(FString::FromInt(InEntry->ItemRef));
+		        })
+		        // .Text(FText::Format(LOCTEXT("Each Generate ID","{0}"), InEntry.Get()))
+		        .OnTextChanged_Lambda([&, InEntry](const FText& InText)->void
+		        {
+		            if(InText.IsNumeric())
+		            {
+		                int32 TargetInputInt = FCString::Atoi(*InText.ToString());
+		                InEntry->ItemRef = TargetInputInt;
+		                CurrentLevelInfomation->MarkPackageDirty();
+		            }
+		        })
+			]
+			+SHorizontalBox::Slot()
+			.AutoWidth()
+			[
+				SNew(STextBlock)
+				.Text(LOCTEXT("NextSplit", "->"))
+				.ColorAndOpacity(FLinearColor(1,1,1,1))
+			]
+		]
+	];
+}
+
+void SWaveSettingsPanel::InitGenerateIDArray()
+{
+	GeneratedIDArray.Empty();
+	for(int32& InIDItem: CurrentWaveInfo->GeneratedID)
+	{
+		GeneratedIDArray.Add(MakeShared<FGenerateIDWidgetItem>(FGenerateIDWidgetItem(InIDItem)));
+	}
+}
+
+FReply SWaveSettingsPanel::OnAddNewGeneratedIDBtnClicked()
+{
+	int32 NewID = 0;
+	CurrentWaveInfo->GeneratedID.Add(NewID);
+	CurrentLevelInfomation->MarkPackageDirty();
+	InitGenerateIDArray();
+	GeneratedIDListTileWidget->RequestListRefresh();
+	return FReply::Handled();
+}
+
+FReply SWaveSettingsPanel::OnDeleteSelectedIDBtnClicked()
+{
+	TArray<FGeneratedIDEntry> SelectedItems = GeneratedIDListTileWidget->GetSelectedItems();
+	if(SelectedItems.Num()==0)
+		return FReply::Handled();
+	for(auto& EachItem:SelectedItems)
+	{
+		CurrentWaveInfo->GeneratedID.RemoveAll([EachItem](int32& TargetItem)->bool
+		{
+			if(&EachItem->ItemRef == &TargetItem)
+				return true;
+			return false;
+		});
+	}
+	InitGenerateIDArray();
+	GeneratedIDListTileWidget->RequestListRefresh();
+	CurrentLevelInfomation->MarkPackageDirty();
+	return FReply::Handled();
 }
 
 #undef LOCTEXT_NAMESPACE
--- Source/TowerDefence/LevelEditor/SWaveSettingsPanel.h
+++ Source/TowerDefence/LevelEditor/SWaveSettingsPanel.h
@@ -1,10 +1,19 @@
 ﻿#pragma once
+#include "Brushes/SlateColorBrush.h"
+#include "Widgets/Views/STileView.h"
 
 class SEditableTextBox;
 struct FEnemyGenerationInfo;
 class ULevelInfomation;
+struct FGenerateIDWidgetItem
+{
+	int32& ItemRef;
+	FGenerateIDWidgetItem(int32& InItem):
+	ItemRef(InItem){}
+};
 class SWaveSettingsPanel:public SCompoundWidget
 {
+	typedef TSharedPtr<FGenerateIDWidgetItem> FGeneratedIDEntry;
 	SLATE_BEGIN_ARGS(SWaveSettingsPanel){}
 	SLATE_END_ARGS()
 public:
@@ -15,6 +24,19 @@ private:
 	ULevelInfomation* CurrentLevelInfomation;
 	TSharedPtr<SEditableTextBox> GrapTimeInputWidget;
 
-
+	// Generated IDs
+	TSharedPtr<STileView<FGeneratedIDEntry>> GeneratedIDListTileWidget;
+	TArray<FGeneratedIDEntry> GeneratedIDArray;
+	FSlateColorBrush GenerateIDItemBorderBrush = FSlateColorBrush(FLinearColor(0,0,0,0));
+	// --------------
+	
 	void OnGrapTimeInputChanged(const FText& InText);
+
+
+	// GeneratedIDs
+	TSharedRef<ITableRow> OnGenerateIDTile(FGeneratedIDEntry InEntry, const TSharedRef<STableViewBase>& TableViewBase);
+	void InitGenerateIDArray();
+	FReply OnAddNewGeneratedIDBtnClicked();
+	FReply OnDeleteSelectedIDBtnClicked();
+	// --------------
 };
